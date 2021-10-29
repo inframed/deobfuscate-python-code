eba89bab47e6123dad83c520650353346cb39cff
lixfr <52087177+lixfr@users.noreply.github.com>
2021-10-29 14:35:12 +0300

    Update deob.py

--- deob.py
+++ deob.py
@@ -1,7 +1,7 @@
 import base64
 import base64, codecs
 
-#add ur code here and remove the last line.
+#add ur code here and remove the last line of the obfuscated code.
 
 with open('output.txt','wb') as output:
   output.write(base64.b64decode(trust))
